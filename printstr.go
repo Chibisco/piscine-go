22830aa7c614a2307d5dde9b1e20fe0dc74e58db
Innocent_Agu <innocentagu1611@gmail.com>
2022-06-30 21:44:28 +0300

    String

--- printstr.go
+++ printstr.go
@@ -1,10 +1,12 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintStr(s string) {
-	for _, word := range s {
-		z01.PrintRune(word)
+	Astr := []rune(s)
+	for _, v := range Astr {
+		z01.PrintRune(v)
 	}
-
 }
