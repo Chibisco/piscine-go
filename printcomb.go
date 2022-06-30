0b35860728038a07b637e05c6a233485fffff003
Innocent_Agu <innocentagu1611@gmail.com>
2022-06-30 18:36:26 +0300

    a

--- printcomb.go
+++ printcomb.go
@@ -1,8 +1,22 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
 func main() {
-	//random comment
+	for i := '0'; i <= '7'; i++ {
+		for j := '1'; j <= '8'; j++ {
+			for k := '2'; k <= '9'; k++ {
+				if i < j && j < k {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					if i < '7' {
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+					}
+				}
+			}
+		}
+	}
 	z01.PrintRune(10)
 }
