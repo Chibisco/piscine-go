649bc2e71d73fabbdc336c86d7f15c575eeb67c1
Innocent_Agu <innocentagu1611@gmail.com>
2022-06-30 22:51:45 +0300

    basic

--- basicatoi.go
+++ basicatoi.go
@@ -1,13 +1,15 @@
 package piscine
 
-import "strconv"
-
 func BasicAtoi(s string) int {
-	i, err := strconv.Atoi(s)
-
-	if err == nil {
-		return i
-	} else {
-		return 0
+	o_number := 0
+	c := 0
+	a_s := []rune(s)
+	for _, word := range a_s {
+		for i := '0'; i < word; i++ {
+			c++
+		}
+		o_number = o_number*10 + c
+		c = 0
 	}
+	return o_number
 }
