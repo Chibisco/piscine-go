2d11a51b3a6f03a4bdbfd7a6341d8a49c5ab1fe2
Innocent_Agu <innocentagu1611@gmail.com>
2022-07-02 01:20:56 +0300

    Comb2

--- printcomb2.go
+++ printcomb2.go
@@ -3,26 +3,32 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j <= '9'; j++ {
-			for k := '0'; k <= '9'; k++ {
-				for l := '0'; l <= '9'; l++ {
-					if i <= j && k < l {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
+					if a <= c && b < d {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
 						z01.PrintRune(32)
-						z01.PrintRune(k)
-						z01.PrintRune(l)
-						if i <= '9' {
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						if (a != '9' || b != '8') || (c != '9' || d != '9') {
 							z01.PrintRune(44)
 							z01.PrintRune(32)
 						}
-
+					} else if a < c && b >= d {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(32)
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						z01.PrintRune(44)
+						z01.PrintRune(32)
 					}
 				}
 			}
 		}
 	}
-
 	z01.PrintRune(10)
 }
