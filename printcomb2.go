c59d5cfdbb5cba05be3593b28dddc7c587017da5
Innocent_Agu <innocentagu1611@gmail.com>
2022-07-02 01:07:47 +0300

    Comb2

--- printcomb2.go
+++ printcomb2.go
@@ -7,14 +7,12 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
-
 					if i <= j && k < l {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(32)
 						z01.PrintRune(k)
 						z01.PrintRune(l)
-
 						if i <= '9' {
 							z01.PrintRune(44)
 							z01.PrintRune(32)
