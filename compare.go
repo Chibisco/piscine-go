348940ed04fb75ab126deb1481a95ed9858944a7
Innocent_Agu <innocentagu1611@gmail.com>
2022-07-05 15:56:54 +0300

    Compare

--- compare.go
+++ compare.go
@@ -1,17 +1,11 @@
 package piscine
 
 func Compare(a, b string) int {
-	sentence1 := []rune(a)
-	sentence2 := []rune(b)
-
-	for i := 0; i < len(a)+len(b); i++ {
-		if len(a) == len(b) && sentence1[i] == sentence2[i] {
-			return 0
-		} else if len(a) != len(b) && sentence1[0] != sentence2[0] {
-			return -1
-		} else if len(a) != len(b) && sentence1[len(a)-1] != sentence2[len(b)-1] {
-			return 1
-		}
+	if a == b {
+		return 0
+	} else if a < b {
+		return -1
+	} else {
+		return 1
 	}
-	return 0
 }
