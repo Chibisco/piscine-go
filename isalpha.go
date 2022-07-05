95dad36d502eb0d96b3a4d2d91f9f33621f771fe
Innocent_Agu <innocentagu1611@gmail.com>
2022-07-05 22:42:50 +0300

    IsAlpha

--- isalpha.go
+++ isalpha.go
@@ -7,8 +7,10 @@ func IsAlpha(s string) bool {
 	for i := range s {
 		cd = i
 	}
-	for i := 0; i < cd; i++ {
-		if (c[i] < 'a' || c[i] > 'z') && (c[i] < 'A' || c[i] > 'Z') && (c[i] < '0' || c[i] > '9') {
+	for i := 0; i <= cd; i++ {
+		if (c[i] < 'a' || c[i] > 'z') &&
+			(c[i] < 'A' || c[i] > 'Z') &&
+			(c[i] < '0' || c[i] > '9') {
 			return false
 		}
 	}
