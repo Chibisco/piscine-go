552d73b590adf06324fbdf5c694082c3480cf167
Innocent_Agu <innocentagu1611@gmail.com>
2022-07-06 21:19:18 +0300

    NBRCA

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -7,27 +7,52 @@ import (
 )
 
 func main() {
-	arguments := os.Args[1:]
-	caps := false
-	for _, v := range arguments {
-		if v == "--upper" {
-			caps = true
+	arg := os.Args[1:]
+	len := 0
+	flag := false
+	for i := range arg {
+		len = i
+	}
+	for _, val := range arg {
+		if val == "--upper" {
+			flag = true
 		}
 	}
-	for _, arg := range arguments {
-		numv := 0
-		for _, v := range arg {
-			numv = numv*10 + int(v-'0')
+
+	if flag {
+		for i := 0; i <= len; i++ {
+			z01.PrintRune(ToUpper(ConvertToLetter(arg[i])))
 		}
-		if numv >= 1 && numv <= 26 {
-			if caps == false {
-				z01.PrintRune(rune(numv + 96))
-			} else if caps == true {
-				z01.PrintRune(rune(numv + 64))
-			} else {
-				z01.PrintRune(' ')
-			}
+		z01.PrintRune(10)
+	} else if len == 0 {
+		z01.PrintRune(10)
+	} else {
+		for i := 0; i <= len; i++ {
+			z01.PrintRune(ConvertToLetter(arg[i]))
 		}
+		z01.PrintRune(10)
 	}
-	z01.PrintRune('\n')
+}
+
+func ConvertToLetter(s string) rune {
+	numb := 0
+	for _, i := range s {
+		count := 0
+		for k := '0'; k < i; k++ {
+			count++
+		}
+		numb = numb*10 + count
+	}
+	numb = numb + 96
+	if numb >= 123 {
+		return ' '
+	}
+	return rune(numb)
+}
+
+func ToUpper(r rune) rune {
+	if r-32 < 65 {
+		return r
+	}
+	return r - 32
 }
