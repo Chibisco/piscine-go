e26fea7217081a169918ec4edadba997d0530623
Innocent_Agu <innocentagu1611@gmail.com>
2022-07-06 12:27:29 +0300

    Pprog

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,10 +6,14 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
-	arg := os.Args
-	for _, w := range arg[0] {
-		z01.PrintRune(w)
+func PrintString(s string) {
+	str := []rune(s)
+	for _, val := range str {
+		z01.PrintRune(val)
 	}
 	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintString(os.Args[0])
 }
