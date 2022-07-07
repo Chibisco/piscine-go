7ae5f3b065d38f527f8f82606a856cc5b9fde065
Innocent_Agu <innocentagu1611@gmail.com>
2022-07-07 17:00:25 +0300

    RotateV

--- rotatevowels/main.go
+++ rotatevowels/main.go
@@ -8,62 +8,83 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	len := 0
-	for range args {
-		len++
+	if len(args) == 0 {
+		z01.PrintRune('\n')
+	} else {
+		wordSlice, charCount, characters := processInput(args)
+		aL := getVowels(characters, charCount)
+		newSlice := mirrorVowels(wordSlice, aL)
+		for _, v := range newSlice {
+			for _, e := range v {
+				z01.PrintRune(e)
+			}
+			z01.PrintRune(' ')
+		}
+		z01.PrintRune('\n')
 	}
-	if len > 0 {
-		vowels := "AEIOUaeiou"
-		str, vow, vow1 := "", "", ""
-		for i, j := range args {
-			str += j
-			if i != len-1 {
-				str += " "
-			}
-			for _, b := range j {
-				for _, c := range vowels {
-					if b == c {
-						vow += string(b)
-					}
-				}
-			}
-		}
-		var lenv int
-		for range vow {
-			lenv++
-		}
-		for i := lenv - 1; i >= 0; i-- {
-			vow1 += string(vow[i])
-		}
-		a, res := 0, ""
-		for _, j := range str {
-			vowel := false
-			for _, b := range vowels {
-				if j == b {
-					res += string(vow1[a])
-					a++
-					vowel = true
-					break
-				}
-			}
-			if !vowel {
-				res += string(j)
-			}
-		}
-		for _, j := range res {
-			z01.PrintRune(j)
-		}
-	}
-	z01.PrintRune('\n')
 }
 
-// ./rotatevowels "Hello World"
-// Hollo Werld
-// ./rotatevowels "HEllO World" "problem solved"
-// Hello Werld problom sOlvEd
-//  ./rotatevowels "str" "shh" "psst"
-// str shh psst
-// ./rotatevowels "happy thoughts" "good luck"
-// huppy thooghts guod lack
-// ./rotatevowels "al's elEphAnt is overly underweight!"
-// il's elephunt es ovirly AndErweaght!
+func processInput(args []string) ([]string, int, string) {
+	result := []string{}
+	characters := ""
+	word := ""
+	wordCount := 0
+	for _, v := range args {
+		for i, e := range v {
+			if e != ' ' && i < len(v) {
+				word += string(e)
+				characters += string(e)
+				wordCount++
+				continue
+			} else {
+				result = append(result, word)
+				word = ""
+			}
+		}
+		result = append(result, word)
+		word = ""
+	}
+	return result, wordCount, characters
+}
+
+func getVowels(s string, charCount int) []string {
+	a := []string{}
+	var loopCount int = charCount
+	for i := 0; i < loopCount; i++ {
+		if isVowel(string(s[i])) {
+			a = append(a, string(s[i]))
+		}
+	}
+	return a
+}
+
+func isVowel(c string) bool {
+	vowels := "AEIOUaeiou"
+	for _, v := range vowels {
+		if c == string(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func mirrorVowels(wS []string, aL []string) []string {
+	mirrorCharCount := len(aL) - 1
+	newWordSlice := wS
+	counter := 0
+	if mirrorCharCount > 0 {
+		for i, v := range wS {
+			currentWord := ""
+			for _, e := range v {
+				if isVowel(string(e)) {
+					currentWord += string(aL[mirrorCharCount-counter])
+					counter++
+				} else {
+					currentWord += string(e)
+				}
+			}
+			newWordSlice[i] = currentWord
+		}
+	}
+	return newWordSlice
+}
